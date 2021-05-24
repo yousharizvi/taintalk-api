96d606a00c5928bd96f5a17021f45719ccb52cb6
Yousha Rizvi <yousharizvi@gmail.com>
2021-05-24 12:55:10 +0500

    root route set

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
 app.use('/api/v1/', indexRouter);
 app.use('/api/v1/user', userRouter);
 
